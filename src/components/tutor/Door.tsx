a704c2614ddc3b6ff4a7548fbfb3e63dc1b7c832
OchaiGinger <153945604+OchaiGinger@users.noreply.github.com>
2025-10-10 16:52:03 +0100

    Update Door.tsx

--- src/components/tutor/Door.tsx
+++ src/components/tutor/Door.tsx
@@ -1,5 +1,4 @@
 import { Text } from "@react-three/drei";
-import * as THREE from "three";
 
 interface DoorProps {
   position: [number, number, number];
@@ -15,20 +14,20 @@ export function Door({ position, label, isClassInSession, onClick }: DoorProps) 
   return (
     <group position={position} onClick={onClick}>
       {/* Frame */}
-      <mesh position={[0, 2, 0]}>
-        <boxGeometry args={[2.4, 4.2, 0.2]} />
+      <mesh position={[0, 1.8, 0]}>
+        <boxGeometry args={[2.4, 3.6, 0.2]} />
         <meshStandardMaterial color="#cccccc" metalness={0.4} roughness={0.5} />
       </mesh>
 
       {/* Door panel */}
-      <mesh position={[0, 2, 0.11]}>
-        <boxGeometry args={[2, 4, 0.1]} />
+      <mesh position={[0, 1.8, 0.11]}>
+        <boxGeometry args={[2, 3.4, 0.1]} />
         <meshStandardMaterial color={doorColor} metalness={0.3} roughness={0.6} />
       </mesh>
 
       {/* Glass window */}
-      <mesh position={[0, 2.5, 0.12]}>
-        <planeGeometry args={[1, 1]} />
+      <mesh position={[0, 2.4, 0.12]}>
+        <planeGeometry args={[1, 0.8]} />
         <meshPhysicalMaterial
           color={glassColor}
           transmission={0.7}
@@ -41,18 +40,18 @@ export function Door({ position, label, isClassInSession, onClick }: DoorProps) 
       </mesh>
 
       {/* Handle */}
-      <mesh position={[0.8, 2, 0.25]} rotation={[Math.PI / 2, 0, 0]}>
+      <mesh position={[0.8, 1.8, 0.25]} rotation={[Math.PI / 2, 0, 0]}>
         <cylinderGeometry args={[0.05, 0.05, 0.4, 16]} />
         <meshStandardMaterial color="#d4af37" metalness={0.8} roughness={0.2} />
       </mesh>
 
       {/* Label */}
-      <Text position={[0, 4.7, 0.1]} fontSize={0.25} color="#222" anchorX="center" anchorY="middle">
+      <Text position={[0, 3.8, 0.1]} fontSize={0.25} color="#222" anchorX="center" anchorY="middle">
         {label}
       </Text>
 
       {/* Light indicator */}
-      <mesh position={[0, 0.2, 0.15]}>
+      <mesh position={[0, 0.15, 0.15]}>
         <circleGeometry args={[0.15, 32]} />
         <meshStandardMaterial
           color={isClassInSession ? "#00ff00" : "#ff0000"}
@@ -63,4 +62,5 @@ export function Door({ position, label, isClassInSession, onClick }: DoorProps) 
     </group>
   );
 }
+
 
