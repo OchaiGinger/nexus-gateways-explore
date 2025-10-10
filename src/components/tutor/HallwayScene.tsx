2468e03d91d25a91b43c36b8007d9beac5c3d0af
OchaiGinger <153945604+OchaiGinger@users.noreply.github.com>
2025-10-10 16:26:26 +0100

    Update HallwayScene.tsx

--- src/components/tutor/HallwayScene.tsx
+++ src/components/tutor/HallwayScene.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { KeyboardControls, Environment, Stars } from "@react-three/drei";
+import { KeyboardControls, Environment, Stars, OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
 import { TutorPlayer } from "./TutorPlayer";
 import { Door } from "./Door";
@@ -14,55 +14,57 @@ const keyboardMap = [
   { name: "right", keys: ["ArrowRight", "KeyD"] },
 ];
 
-const rooms = [
-  { name: "Ward A", position: [-6, 2, -20], inSession: true },
-  { name: "Ward B", position: [6, 2, -20], inSession: false },
-  { name: "Pharmacy", position: [-6, 2, -10], inSession: true },
-  { name: "Lab", position: [6, 2, -10], inSession: false },
-  { name: "Reception", position: [-6, 2, 0], inSession: true },
-  { name: "Radiology", position: [6, 2, 0], inSession: false },
-  { name: "ICU", position: [-6, 2, 10], inSession: false },
-  { name: "Doctor's Office", position: [6, 2, 10], inSession: true },
+const classrooms = [
+  { name: "Class 1A", position: [-9.85, 1.5, -20], rotationY: Math.PI / 2 },
+  { name: "Class 1B", position: [9.85, 1.5, -15], rotationY: -Math.PI / 2 },
+  { name: "Class 2A", position: [-9.85, 1.5, -10], rotationY: Math.PI / 2 },
+  { name: "Class 2B", position: [9.85, 1.5, -5], rotationY: -Math.PI / 2 },
+  { name: "Class 3A", position: [-9.85, 1.5, 0], rotationY: Math.PI / 2 },
+  { name: "Class 3B", position: [9.85, 1.5, 5], rotationY: -Math.PI / 2 },
+  { name: "Class 4A", position: [-9.85, 1.5, 10], rotationY: Math.PI / 2 },
+  { name: "Class 4B", position: [9.85, 1.5, 15], rotationY: -Math.PI / 2 },
 ];
 
-function HospitalHallway() {
+function SchoolHallway() {
   return (
     <group>
       {/* Floor */}
       <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} receiveShadow>
         <planeGeometry args={[20, 60]} />
-        <meshStandardMaterial color="#d9e0e8" metalness={0.1} roughness={0.8} />
+        <meshStandardMaterial color="#d8d8d8" roughness={0.7} metalness={0.2} />
       </mesh>
 
       {/* Ceiling */}
-      <mesh rotation={[Math.PI / 2, 0, 0]} position={[0, 10, 0]}>
+      <mesh rotation={[Math.PI / 2, 0, 0]} position={[0, 8, 0]}>
         <planeGeometry args={[20, 60]} />
-        <meshStandardMaterial color="#f5f5f5" roughness={1} />
+        <meshStandardMaterial color="#fafafa" roughness={1} />
       </mesh>
 
-      {/* Left & Right Walls */}
-      <mesh position={[-10, 5, 0]}>
-        <boxGeometry args={[0.3, 10, 60]} />
-        <meshStandardMaterial color="#e0e0e0" />
-      </mesh>
-      <mesh position={[10, 5, 0]}>
-        <boxGeometry args={[0.3, 10, 60]} />
-        <meshStandardMaterial color="#e0e0e0" />
+      {/* Side walls */}
+      <mesh position={[-10, 4, 0]}>
+        <boxGeometry args={[0.3, 8, 60]} />
+        <meshStandardMaterial color="#f0f0f0" />
       </mesh>
 
-      {/* Doors */}
-      {rooms.map((room, i) => (
+      <mesh position={[10, 4, 0]}>
+        <boxGeometry args={[0.3, 8, 60]} />
+        <meshStandardMaterial color="#f0f0f0" />
+      </mesh>
+
+      {/* Doors on both sides */}
+      {classrooms.map((room, i) => (
         <Door
           key={i}
           position={room.position as [number, number, number]}
+          rotation={[0, room.rotationY, 0]}
           label={room.name}
-          isClassInSession={room.inSession}
+          isClassInSession={Math.random() > 0.5}
         />
       ))}
 
       {/* Lighting */}
-      <ambientLight intensity={0.8} />
-      <directionalLight position={[10, 20, 10]} intensity={1.2} castShadow />
+      <ambientLight intensity={0.7} />
+      <directionalLight position={[5, 10, 5]} intensity={1.2} castShadow />
       <Environment preset="city" />
     </group>
   );
@@ -72,19 +74,30 @@ export function HallwayScene() {
   const [playerPosition, setPlayerPosition] = useState(new THREE.Vector3(0, 1, 20));
 
   return (
-    <div style={{ width: "100vw", height: "100vh", position: "fixed", top: 0, left: 0 }}>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        backgroundColor: "black",
+      }}
+    >
       <KeyboardControls map={keyboardMap}>
-        <Canvas shadows camera={{ fov: 75, position: [0, 2, 5] }}>
-          <Stars radius={80} depth={20} count={3000} factor={4} fade speed={1} />
-          <HospitalHallway />
+        <Canvas shadows camera={{ position: [0, 2, 5], fov: 75 }}>
+          <Stars radius={80} depth={20} count={2000} factor={4} fade speed={1} />
+          <OrbitControls enablePan={false} enableZoom={false} />
+          <SchoolHallway />
           <TutorPlayer
             modelUrl="/models/character.glb"
             onPositionChange={setPlayerPosition}
-            doors={rooms}
+            doors={classrooms}
           />
         </Canvas>
       </KeyboardControls>
     </div>
   );
 }
+
 
